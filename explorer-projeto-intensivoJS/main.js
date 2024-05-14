1defa27ab1f068ba29b32139bfd3c86756c7aece
Nataly Caires <59523655+natcaires@users.noreply.github.com>
2024-05-13 21:19:49 -0400

    Add files via upload

---
+++ explorer-projeto-intensivoJS/main.js
@@ -0,0 +1,95 @@
+/* 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador. */
+// alert("Hello World!")
+
+
+/* 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas. */
+// let number1 = 5;
+// let number2 = 2;
+// const sumResult = number1 + number2;
+// console.log(sumResult);
+
+
+/* 3. Crie um script que declare uma variável e verifique se o seu valor é um número. 
+Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".*/
+// let data1 = 1;
+
+// if (typeof data1 === 'number') {
+//   console.log("It's a number.")
+// } else {
+//   console.log("It's not a number.")
+// }
+
+
+
+/* 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. 
+Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".*/
+// let data2 = "hello";
+
+// if (typeof data2 === 'string') {
+//   console.log("It's a string.")
+// } else {
+//   console.log("It's not a string.")
+// }
+
+
+
+/* 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".*/
+// let data3 = true;
+
+// if (typeof data3 === 'boolean') {
+//   console.log("It's a boolean")
+// } else {
+//   console.log("It's not a boolean")
+// }
+
+
+
+/* 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.*/
+// let firstNumber = 6;
+// let secondNumber = 4;
+
+// const subtractionResult = firstNumber - secondNumber;
+// console.log(subtractionResult);
+
+
+
+/* 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas. */
+// let num1 = 5;
+// let num2 = 2;
+
+// const multiplication = num1 * num2;
+// console.log(multiplication);
+
+
+
+/* 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.*/
+// let numero1 = 10;
+// let numero2 = 2;  
+
+// const division = numero1 / numero2;
+// console.log(division);
+
+
+
+/* 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. 
+Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".*/
+// let isItEven = 10;
+
+// if (isItEven % 2 === 0) {
+//   console.log("It's an even number.")
+// } else {
+//   console.log("It's not an even number.")
+// }
+
+
+
+/* 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. 
+Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".*/
+// let isItOdd = 15;
+
+// if (isItOdd % 2 != 0) {
+//   console.log("It's an odd number.") 
+// } else [
+//   console.log("It's not an odd number.")
+// ]
